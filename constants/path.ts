4603e0b5ae78de7b3effeccc3af857585279f36d
Taishi Kobari <70504137+TaishiKobari@users.noreply.github.com>
2022-06-08 16:20:44 +0900

    path.ts

---
+++ constants/path.ts
@@ -0,0 +1,6 @@
+export const HOME_PATH = "/"
+export const LOGIN_PATH = "/api/auth/signin"
+export const EXTENSIONS_PATH = "/extensions"
+
+export const EXTENSIONS_API = "/api/extensions"
+export const EXTENSIONS_IMPORT_API = "/api/extensions/import"
